cb3b2542dc32cc1d6eba5500f11e995d73901eb7
Carter Parrett <cparrett94@hotmail.com>
2020-07-11 02:30:37 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,17 @@
 def using_until(force)
   force = 6
-  until force < 10
+  until force == 10
   puts "Wingardium Leviosa"
   force += 1
   end
 end
 
+
+def using_until
+  levitation_force = 6
+  until levitation_force == 10
+  puts "Wingardium Leviosa"
+  levitation_force += 1
+  end
+end
 
